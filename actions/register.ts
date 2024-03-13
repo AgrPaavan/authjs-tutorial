4289dbfea1f6877c9c461125d0607cb1f81254a0
Paavan Agrawal <33314332+AgrPaavan@users.noreply.github.com>
2024-03-14 00:18:03 +0530

    Add prisma with neon.tech for db

--- actions/register.ts
+++ actions/register.ts
@@ -1,12 +1,33 @@
 "use server";
 
-import { RegisterSchema } from "@/schemas";
+import bcrypt from "bcrypt";
 import * as z from "zod";
+
+import { getUserByEmail } from "@/data/user";
+import { db } from "@/lib/db";
+import { RegisterSchema } from "@/schemas";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
 
   if (!validatedFields.success) return { error: "Invalid fields!" };
 
-  return { success: "Email sent!" };
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const exisitngUser = await getUserByEmail(email);
+
+  if (exisitngUser) return { error: "Email already in use!" };
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // TODO: Send verification email
+
+  return { success: "User created!" };
 };
